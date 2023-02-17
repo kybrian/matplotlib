5c1b57ce005428fb04083349b9f091ae694dad7f
kybrian <kybrian.se@gmail.com>
2023-02-17 08:27:38 +0300

    Initial commit

---
+++ 01_pyplot.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+
+# Draaw a line in a diagram from position (0,0) to position (6, 250)
+import numpy as np 
+
+xpoints = np.array([0, 6])
+ypoints = np.array([0, 250])
+
+plt.plot(xpoints, ypoints)
+plt.show()
